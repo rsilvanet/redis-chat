2f2689f7e799d187b228b73e33d795e7e1dddc55
Robson <me@rsilva.net>
2019-03-26 22:06:22 -0300

    Initial projects structure created

---
+++ src/RedisChat.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedisChat.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/RedisChat.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RedisChat.Core
+{
+    public class Class1
+    {
+    }
+}
